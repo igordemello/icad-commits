369abfeb5cdb11857ee545706b364c8f4ce36449
Igor Mello <igor.oliveira.mello@gmail.com>
2026-03-27 11:22:04 -0300

    Add webhook url

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const DISCORD_WEBHOOK_URL = "COLOCA_AQUI";
+const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1487094062589935707/2B83r4RPkrEi6GTK809l094rjV-ZYwmKdYxeAHieBAX91VgF2KCKwCOY8GKCb8Zv8mXH";
 
 app.post("/github", async (req, res) => {
   const data = req.body;
